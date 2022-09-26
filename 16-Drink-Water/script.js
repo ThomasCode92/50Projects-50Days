9d0c2a2fb7a0a83a81b6c4e2c6976446750d08fd
ThomasCode92 <thomascode92@hotmail.com>
2022-09-26 12:45:20 +0200

    Update Big Cup

--- 16-Drink-Water/script.js
+++ 16-Drink-Water/script.js
@@ -8,8 +8,12 @@ smallCups.forEach((cup, idx) => {
   cup.addEventListener('click', () => highlightCups(idx));
 });
 
+updateBigCup();
+
 function highlightCups(cupIndex) {
-  if (
+  if (cupIndex === 7 && smallCups[cupIndex].classList.contains('full'))
+    cupIndex--;
+  else if (
     smallCups[cupIndex].classList.contains('full') &&
     !smallCups[cupIndex].nextElementSibling.classList.contains('full')
   ) {
@@ -23,4 +27,28 @@ function highlightCups(cupIndex) {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
