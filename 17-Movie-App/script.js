e72048816ba49f7504b839a5d8266830f8c5ebc4
ThomasCode92 <thomascode92@hotmail.com>
2022-09-27 12:43:23 +0200

    Adding Movies To The DOM

--- 17-Movie-App/script.js
+++ 17-Movie-App/script.js
@@ -15,7 +15,42 @@ async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  console.log(data.results);
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach(movie => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+      <img src="${IMG_PATH + poster_path}" alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
 }
 
 form.addEventListener('submit', e => {
