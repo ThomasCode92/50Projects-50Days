42de5b4acda3ae0a15b2b97da923616c2c6fd1bf
ThomasCode92 <thomascode92@hotmail.com>
2022-10-03 08:04:09 +0200

    Mouse Events & Drawing

--- 22-Drawing-App/script.js
+++ 22-Drawing-App/script.js
@@ -2,10 +2,38 @@ const canvas = document.getElementById('canvas');
 
 let x;
 let y;
+let isPressed = false;
 let size = 20;
 let color = 'black';
 
 const context = canvas.getContext('2d');
+
+canvas.addEventListener('mousedown', event => {
+  isPressed = true;
+
+  x = event.offsetX;
+  y = event.offsetY;
+});
+
+document.addEventListener('mouseup', event => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', event => {
+  if (isPressed) {
+    const x2 = event.offsetX;
+    const y2 = event.offsetY;
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
 
 function drawCircle(x, y) {
   context.beginPath();
@@ -19,7 +47,7 @@ function drawLine(x1, y1, x2, y2) {
   context.moveTo(x1, y1);
   context.lineTo(x2, y2);
   context.strokeStyle = color;
-  context.lineWidth = size;
+  context.lineWidth = size * 2;
   context.stroke();
 }
 
