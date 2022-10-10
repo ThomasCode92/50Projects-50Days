7a78a2af5feba05f71e9391c12e8c17223027274
ThomasCode92 <thomascode92@hotmail.com>
2022-10-10 08:08:03 +0200

    Dynamic Profile Cards

--- 29-Github-Profiles/script.js
+++ 29-Github-Profiles/script.js
@@ -1,7 +1,10 @@
 const formElement = document.getElementById('form');
 const searchInputElement = document.getElementById('search');
+const mainElement = document.getElementById('main');
 
 const API_URL = 'https://api.github.com/users/';
+
+mainElement.innerHTML = '';
 
 formElement.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -17,8 +20,45 @@ formElement.addEventListener('submit', function (event) {
 async function getUser(username) {
   try {
     const { data } = await axios(API_URL + username);
-    console.log(data);
+    createUserCard(data);
   } catch (error) {
-    console.log(error);
+    if (error.response.status === 404) {
+      createErrorCard('No profile with this username!');
+    }
   }
+}
+
+function createUserCard(data) {
+  const cardElement = `
+    <div class="card">
+      <div>
+        <img
+          class="avatar"
+          src="${data.avatar_url}"
+          alt="${data.name}" />
+      </div>
+      <div class="user-info">
+        <h2>${data.name}</h2>
+        <p>${data.bio}</p>
+        <ul>
+          <li>${data.followers} <strong>Followers</strong></li>
+          <li>${data.following} <strong>Following</strong></li>
+          <li>${data.public_repos} <strong>Repos</strong></li>
+        </ul>
+        <div id="repos"></div>
+      </div>
+    </div>
+  `;
+
+  mainElement.innerHTML = cardElement;
+}
+
+function createErrorCard(message) {
+  const cardElement = `
+    <div class="card">
+        <h1>${message}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardElement;
 }
