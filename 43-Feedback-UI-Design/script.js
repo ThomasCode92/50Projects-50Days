10991b3083faf32ca451870b4b5b1cec954e9d61
ThomasCode92 <thomascode92@hotmail.com>
2022-10-30 14:03:52 +0100

    Feedback-UI-Design

---
+++ 43-Feedback-UI-Design/script.js
@@ -0,0 +1,41 @@
+const ratingElements = document.querySelectorAll('.rating');
+const ratingsContainerElement = document.querySelector('.ratings-container');
+const sendBtnElement = document.querySelector('#send');
+const panelElement = document.querySelector('#panel');
+
+let selectedRating = 'Satisfied';
+
+ratingsContainerElement.addEventListener('click', event => {
+  if (
+    event.target.parentNode.classList.contains('rating') &&
+    event.target.nextElementSibling
+  ) {
+    removeActive();
+    event.target.parentNode.classList.add('active');
+    selectedRating = event.target.nextElementSibling.innerHTML;
+  } else if (
+    event.target.parentNode.classList.contains('rating') &&
+    event.target.previousSibling &&
+    event.target.previousElementSibling.nodeName === 'IMG'
+  ) {
+    removeActive();
+    event.target.parentNode.classList.add('active');
+    selectedRating = event.target.innerHTML;
+  }
+});
+
+sendBtnElement.addEventListener('click', e => {
+  panelElement.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support</p>
+  `;
+});
+
+function removeActive() {
+  for (let i = 0; i < ratingElements.length; i++) {
+    ratingElements[i].classList.remove('active');
+  }
+}
