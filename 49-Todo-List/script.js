9c1ca2d121e688aef9f02f4cd69aa174d10cb5cd
ThomasCode92 <thomascode92@hotmail.com>
2022-11-06 00:06:57 +0100

    Saving Todos to Local Storage

--- 49-Todo-List/script.js
+++ 49-Todo-List/script.js
@@ -2,6 +2,12 @@ const formElement = document.getElementById('form');
 const inputElement = document.getElementById('input');
 const todosListElement = document.getElementById('todos');
 
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if (todos) {
+  todos.forEach(todo => addTodo(todo));
+}
+
 formElement.addEventListener('submit', event => {
   event.preventDefault();
   addTodo();
@@ -24,15 +30,34 @@ function addTodo(todo) {
 
     todoElement.addEventListener('click', () => {
       todoElement.classList.toggle('completed');
+      updateLocalStorage();
     });
 
     todoElement.addEventListener('contextmenu', event => {
       event.preventDefault();
       todoElement.remove();
+      updateLocalStorage();
     });
 
     todosListElement.appendChild(todoElement);
 
     inputElement.value = '';
+
+    updateLocalStorage();
   }
+}
+
+function updateLocalStorage() {
+  const todosEl = document.querySelectorAll('li');
+
+  const todos = [];
+
+  todosEl.forEach(todoElement => {
+    todos.push({
+      text: todoElement.innerText,
+      completed: todoElement.classList.contains('completed'),
+    });
+  });
+
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
